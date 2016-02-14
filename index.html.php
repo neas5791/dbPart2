f1d3e308be1f837b9cefeea05a2fdf30366e28c6
Sean Matkovich <sean.matkovich@gmail.com>
2016-02-14 21:22:28 +1100

    moved navigations to include/ folder, moved script.js to js/ folder

--- index.html.php
+++ index.html.php
@@ -8,13 +8,13 @@
 		<link rel="stylesheet" href="smartmenus/sm-simple.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 		<script src="smartmenus/jquery.smartmenus.min.js"></script>
-		<script type="text/javascript" src="script.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
 
 	</head>
 	<body>
 		<div id="wrapper">
 			<!-- include the navigation bar -->
-			<div id="nav"><?php include 'navigation.inc.html'; ?></div>
+			<div id="nav"><?php include 'include/navigation.inc.html'; ?></div>
 			<div id="content">
 				<p>Welcome to the Transcrete Production database.<br>Please login to access.</p>
 				<div id="filter"></div>
